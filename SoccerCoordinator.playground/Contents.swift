75ba4e4567d276c8a2b90856586e459469b80174
James McMillan <james_mcmillan@icloud.com>
2016-06-17 22:41:42 -0700

    completed step 1, inserting player data into a dictionary  collection

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,27 @@
-//: Playground - noun: a place where people can play
+//Soccer Coordinator : Treehouse iOS TechDegree Project 1
 
-import UIKit
+//Step One: Player Data into a collection
 
-var str = "Hello, playground"
+var players = [Dictionary<String, Any>]()
 
-//James' first comment
+//Appending individual player data into players dictionary
+
+players.append(["name" : "Joe Smith", "height": 42, "experience": true, "guardian": "Jim and Jan Smith"])
+players.append(["name" : "Jill Tanner", "height": 36, "experience": true, "guardian": "Clara Tanner"])
+players.append(["name" : "Bill Bon", "height": 43, "experience": true, "guardian": "Sara and Jenny Bon"])
+players.append(["name" : "Eva Gordon", "height": 45, "experience": false, "guardian": "Wendy and Mike Gordon"])
+players.append(["name" : "Matt Gill", "height": 40, "experience": false, "guardian": "Charles and Sylvia Gill"])
+players.append(["name" : "Kimmy Stein", "height": 41, "experience": false, "guardian": "Bill and Hillary Stein"])
+players.append(["name" : "Sammy Adams", "height": 45, "experience": false, "guardian": "Jeff Adams"])
+players.append(["name" : "Karl Saygan", "height": 42, "experience": true, "guardian": "Heather Bledsoe"])
+players.append(["name" : "Suzane Greenberg", "height": 44, "experience": true, "guardian": "Henrietta Dumas"])
+players.append(["name" : "Sal Dali", "height": 41, "experience": false, "guardian": "Gala Dali"])
+players.append(["name" : "Joe Kavalier", "height": 39, "experience": false, "guardian": "Sam and Elaine Kavalier"])
+players.append(["name" : "Ben Finkelstein", "height": 44, "experience": false, "guardian": "Aaron and Jill Finkelstein"])
+players.append(["name" : "Diego Soto", "height": 41, "experience": true, "guardian": "Robin and Sarika Soto"])
+players.append(["name" : "Chloe Alaska", "height": 47, "experience": false, "guardian": "David and Jamie Alaska"])
+players.append(["name" : "Arnold Willis", "height": 43, "experience": false, "guardian": "Claire Willis"])
+players.append(["name" : "Phillip Helm", "height": 44, "experience": true, "guardian": "Thomas Helm and Eva Jones"])
+players.append(["name" : "Les Clay", "height": 42, "experience": true, "guardian": "Wynonna Brown"])
+players.append(["name" : "Herschel Krustofski", "height": 45, "experience": true, "guardian": "Hyman and Rachel Krustofski"])
+
