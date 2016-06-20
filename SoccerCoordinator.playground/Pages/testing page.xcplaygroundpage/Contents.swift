20e98873327064e4aa1ce650b128471e90e700cf
James McMillan <jmcmillan@hri.uci.edu>
2016-06-20 15:44:20 -0700

    added a testing page to the SoccerCoordinator playground in order to test out some code I wanted to try. Changed data set so that it was a immutable array of dictionaries for better organization and sorting, grouping of data.

---
+++ SoccerCoordinator.playground/Pages/testing page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+let allPlayers: [[String : Any]] = [
+
+["name" : "Joe Smith", "height": 42.0, "experience": true, "guardian": "Jim and Jan Smith"],
+["name" : "Jill Tanner", "height": 36.0, "experience": true, "guardian": "Clara Tanner"],
+["name" : "Bill Bon", "height": 43.0, "experience": true, "guardian": "Sara and Jenny Bon"],
+["name" : "Eva Gordon", "height": 45.0, "experience": false, "guardian": "Wendy and Mike Gordon"],
+["name" : "Matt Gill", "height": 40.0, "experience": false, "guardian": "Charles and Sylvia Gill"],
+["name" : "Kimmy Stein", "height": 41.0, "experience": false, "guardian": "Bill and Hillary Stein"],
+["name" : "Sammy Adams", "height": 45.0, "experience": false, "guardian": "Jeff Adams"],
+["name" : "Karl Saygan", "height": 42.0, "experience": true, "guardian": "Heather Bledsoe"],
+["name" : "Suzane Greenberg", "height": 44.0, "experience": true, "guardian": "Henrietta Dumas"],
+["name" : "Sal Dali", "height": 41.0, "experience": false, "guardian": "Gala Dali"],
+["name" : "Joe Kavalier", "height": 39.0, "experience": false, "guardian": "Sam and Elaine Kavalier"],
+["name" : "Ben Finkelstein", "height": 44.0, "experience": false, "guardian": "Aaron and Jill Finkelstein"],
+["name" : "Diego Soto", "height": 41.0, "experience": true, "guardian": "Robin and Sarika Soto"],
+["name" : "Chloe Alaska", "height": 47.0, "experience": false, "guardian": "David and Jamie Alaska"],
+["name" : "Arnold Willis", "height": 43.0, "experience": false, "guardian": "Claire Willis"],
+["name" : "Phillip Helm", "height": 44.0, "experience": true, "guardian": "Thomas Helm and Eva Jones"],
+["name" : "Les Clay", "height": 42.0, "experience": true, "guardian": "Wynonna Brown"],
+["name" : "Herschel Krustofski", "height": 45.0, "experience": true, "guardian": "Hyman and Rachel Krustofski"]
+
+]
